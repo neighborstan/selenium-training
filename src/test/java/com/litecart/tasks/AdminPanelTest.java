63db897936a9e85e4b449b8892e09d501541329e
Konstantin B <prim.postbox@gmail.com>
2019-02-06 23:16:44 +1000

    First commit by selenium course

---
+++ src/test/java/com/litecart/tasks/AdminPanelTest.java
@@ -0,0 +1,73 @@
+package com.litecart.tasks;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class AdminPanelTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void myFirstTest() {
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+
+        int numMainMenu = getNumberOfElementsFound(By.cssSelector("#app- > a"));
+        for (int i = 0; i < numMainMenu; i++) {
+            getElementWithIndex(By.cssSelector("#app- > a"), i).click();
+            driver.findElement(By.cssSelector("#content h1"));
+
+            int numSubMenu = getNumberOfElementsFound(By.cssSelector("ul.docs li[id^=doc-] a"));
+            for (int j = 0; j < numSubMenu; j++) {
+                getElementWithIndex(By.cssSelector("ul.docs li[id^=doc-] a"), j).click();
+                driver.findElement(By.cssSelector("#content h1"));
+
+            }
+        }
+    }
+
+    public int getNumberOfElementsFound(By by) {
+        return driver.findElements(by).size();
+    }
+
+    public WebElement getElementWithIndex(By by, int pos) {
+        return driver.findElements(by).get(pos);
+    }
+
+    private boolean existsElement(String locator) {
+        try {
+            driver.findElement(By.cssSelector(locator));
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+        return true;
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+
+}
---
+++ src/test/java/com/litecart/tasks/StickersTest.java
@@ -0,0 +1,44 @@
+package com.litecart.tasks;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+
+public class StickersTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void myFirstTest() {
+        driver.get("http://localhost/litecart/");
+
+        List<WebElement> products = driver.findElements(By.cssSelector("li.product"));
+        for (WebElement product : products) {
+            assertEquals(product.findElements(By.cssSelector("div.image-wrapper div.sticker")).size(), 1);
+        }
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+
+}
